b75f7b1150245fc815e297dde847639f5284f94a
IndraP24 <indrashis985@gmail.com>
2021-10-06 12:34:00 +0530

    Update fundamentals

--- University Course - NLP/fundamentals.py
+++ University Course - NLP/fundamentals.py
@@ -1,8 +1,11 @@
-#%% Import and downloads
-import nltk
-nltk.download()
+'''Getting Started'''
+"""import nltk
+nltk.download()"""
+
+# %% Import and downloads
 # %%
 from nltk.book import *
+import nltk
 texts()
 # Output:
 
@@ -20,11 +23,13 @@ texts()
 # text8: Personals Corpus
 # text9: The Man Who Was Thursday by G . K . Chesterton 1908
 
+
+'''Searching Text'''
 # %%
 text1.concordance("monstrous")
 #  Output:
 # Displaying 11 of 11 matches:
-# ong the former , one was of a most monstrous size . ... This came towards us , 
+# ong the former , one was of a most monstrous size . ... This came towards us ,
 # ON OF THE PSALMS . " Touching that monstrous bulk of the whale or ork we have r
 # ll over with a heathenish array of monstrous clubs and spears . Some were thick
 # d as you gazed , and wondered what monstrous cannibal and savage could ever hav
@@ -33,7 +38,7 @@ text1.concordance("monstrous")
 # th of Radney .'" CHAPTER 55 Of the Monstrous Pictures of Whales . I shall ere l
 # ing Scenes . In connexion with the monstrous pictures of whales , I am strongly
 # ere to enter upon those still more monstrous stories of them which are to be fo
-# ght have been rummaged out of this monstrous cabinet there is no telling . But 
+# ght have been rummaged out of this monstrous cabinet there is no telling . But
 # of Whale - Bones ; for Whales of a monstrous size are oftentimes cast up dead u
 # %%
 text4.concordance("advantage")
@@ -42,18 +47,18 @@ text4.concordance("advantage")
 # e and happiness ; between duty and advantage ; between the genuine maxims of an
 # uch cases choice would have little advantage to boast of over lot or chance . S
 #  honorable treaty , and with great advantage to the original States ; the State
-# ve to carry them into effect . The advantage of these fortifications and of an 
+# ve to carry them into effect . The advantage of these fortifications and of an
 # l peace a band of adventurers took advantage of this conflict and of the facili
 # oans may be resorted to with great advantage . I am equally well satisfied , as
 # be considered it would appear that advantage must result from the observance of
 #  interests can rightfully claim an advantage over the others , or to be enriche
 # by the conviction that no apparent advantage can be purchased at a price so dea
 # m no exception . Unwilling to take advantage of the fortune of war against a si
-# obligations or to obtain an unjust advantage over others . They will presently 
+# obligations or to obtain an unjust advantage over others . They will presently
 # es of other powers into commercial advantage to ourselves . We have a just righ
-# No political party can long pursue advantage at the expense of public honor or 
+# No political party can long pursue advantage at the expense of public honor or
 #  prevent other nations from taking advantage of us and of our inability to defe
-# d . We desire neither conquest nor advantage . We wish nothing that can be had 
+# d . We desire neither conquest nor advantage . We wish nothing that can be had
 # servations no special privilege or advantage but only to clarify our relation t
 # we have fought not for our selfish advantage , but to help others resist aggres
 # people . Let us put aside personal advantage , so that we can feel the pain and
@@ -77,28 +82,97 @@ text2.common_contexts(["monstrous", "very"])
 # a_pretty am_glad a_lucky is_pretty be_glad
 
 # %%
-text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+text4.dispersion_plot(
+    ["citizens", "democracy", "freedom", "duties", "America"])
 # Output
 # dispersion plot
-# %%
+
+
+'''Counting Vocabulary'''
+# %% Count the length of a text in terms of the words and punctuations
 len(text3)
 
-# %%
+# Output:
+# 44764
+
+# %% Wrapping sorted() around set(text3) -> List of Vocabulary Items
 sorted(set(text3))
+
+# Output:
+# ['!',
+#  "'",
+#  '(',
+#  ')',
+#  ',',
+#  ',)',
+#  '.',
+#  '.)',
+#  ':',
+#  ';',
+#  ';)',
+#  '?',
+#  '?)',
+#  'A',
+#  'Abel',
+#  'Abelmizraim',
+#  'Abidah',
+#  'Abide',
+#  'Abimael',
+#  'Abimelech',
+#  'Abr',
+#  'Abrah',
+#  'Abraham',
+#  'Abram',
+#  'Accad',
+#  ...
+#  ...
+#  'co',
+#  'coat',
+#  'coats',
+#  'coffin',
+#  'cold',
+#  ...]
+
+# %% Check the size of the vocabulary
+len(set(text3))
+
+# Output:
+# 2789
+
+# %% Measure of Lexical richness of the text
+len(set(text3)) / len(text3)
+
+# Output
+# 0.06230453042623537
+# This means that the number of distinct words is just 6% of the total number of the words
+
+# %% Measure of Lexical Richness of a word
+
+100 * text4.count('a') / len(text4)
+
+# Output:
+# 1.457973123627309
+# This is the percentage of text4 taken up by the word
+
+# %% Methods to automate the above
+
+
+def lexical_diversity(text) -> float:
+    """Measure of Lexical richness of the text as a whole."""
+    return len(set(text)) / len(text)
+
+
+def word_percentage(word: str, text) -> float:
+    """Measure of Lexical richness of a word in a text."""
+    count = text.count(word)
+    total = len(text)
+    return 100 * count / total
+
+
 # %%
-text3.count("smote")
-# %%
-fdist1 = FreqDist(text1)
-print(fdist1)
-fdist1
-# %%
-[w for w in sent7 if len(w) < 4]
-# %%
-[w for w in sent7 if len(w) != 4]
-# %%
-sorted(w for w in set(text1) if w.endswith('ableness'))
-# %%
-sorted(term for term in set(text4) if 'ight' in term)
-# %%
-sorted(wd for wd in set(text3) if wd.istitle() and len(wd) > 10)
+print(f"The Lexical Diversity of text3 is: {lexical_diversity(text3)}")
+print(f"The Lexical Diversity of text6 is: {lexical_diversity(text6)}")
+print(
+    f"The Lexical Diversity percentage of a in text4 is: {word_percentage('a', text4)}")
+
 # %%
